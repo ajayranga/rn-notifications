06ddd22b90c88f24dd4e65cbcbc57e8a711be540
ajay <ramgaa307@gmail.com>
2024-02-14 15:42:20 +0530

    basic notification done

--- App.tsx
+++ App.tsx
@@ -1,31 +1,56 @@
+import notifee from '@notifee/react-native';
 import React from 'react';
 import {Button, SafeAreaView, Text} from 'react-native';
-import notifee from '@notifee/react-native';
+import OnePiecePic from './OnePiece.jpeg';
 
 const App = () => {
   async function onDisplayNotification() {
-    // Request permissions (required for iOS)
-    await notifee.requestPermission();
-
-    // Create a channel (required for Android)
-    const channelId = await notifee.createChannel({
-      id: 'default',
-      name: 'Default Channel',
-    });
-
-    // Display a notification
-    await notifee.displayNotification({
-      title: 'Notification Title',
-      body: 'Main body content of the notification',
-      android: {
-        channelId,
-        smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
-        // pressAction is needed if you want the notification to open the app when pressed
-        pressAction: {
-          id: 'default',
+    try {
+      // Request permissions (required for iOS)
+      await notifee.requestPermission({
+        alert: true,
+        sound: true,
+      });
+      notifee.setBadgeCount(1).then(() => console.log('Badge count set!'));
+      await notifee.setNotificationCategories([
+        {
+          id: 'post',
+          actions: [
+            {
+              id: 'like',
+              title: 'Like Post',
+            },
+            {
+              id: 'dislike',
+              title: 'Dislike Post',
+            },
+          ],
         },
-      },
-    });
+      ]);
+
+      // Display a notification
+      await notifee.displayNotification({
+        title: 'Notification Title',
+        body: 'Main body content of the notification',
+        ios: {
+          categoryId: 'post',
+          attachments: [
+            {
+              url: OnePiecePic,
+            },
+          ],
+          foregroundPresentationOptions: {
+            badge: true,
+            sound: true,
+            banner: true,
+            list: true,
+          },
+          sound: './Assets/DenDenMushi.wav', // not working
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
